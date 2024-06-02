72062cab5f3e80c3bc050ddc021038408f129a68
E-corp Company Group <116536027+E-CorpCompanyGroup@users.noreply.github.com>
2024-06-02 23:48:01 +0300

    Add files via upload

---
+++ App/App.js
@@ -0,0 +1,36 @@
+APPMODE('','','#121212');
+
+APPNAME('Wallet');
+
+NETWORKSTATE((data)=>{
+
+    WIDGET(`
+
+      <img class='LaoderIcon' src='${IMAGEPATH}app_icon.png'/>
+
+    `);
+
+    const LaoderIcon=document.querySelector('.LaoderIcon');
+
+    colorChange(LaoderIcon);
+
+    setTimeout(() => {
+
+        MODULE('../../Connection/Connection.js','CONNECTION',(CONNECTION) => {CONNECTION()});
+          
+    }, 10);
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+          navigator.serviceWorker.register('/service-worker.js')
+            .then(function(registration) {
+              console.log('Service worker registration successful:', registration);
+            })
+            .catch(function(error) {
+              console.log('Service worker registration failed:', error);
+            });
+        });
+    }
+
+});
+
