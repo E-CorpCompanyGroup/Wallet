1617e97cc1657ba26b81196ef3bb18afc0da21e5
E-corp Company Group <116536027+E-CorpCompanyGroup@users.noreply.github.com>
2024-06-03 17:13:27 +0000

    LAUNCH ONE WALLET

--- Project/LoginPage/LoginPage.js
+++ Project/LoginPage/LoginPage.js
@@ -1,66 +1,37 @@
 import { HOMEPAGE } from "../HomePage/HomePage.js";
 
-export const LOGINPAGE=()=>{
+export const LOGINPAGE = () => {
 
     WIDGET(`
-
         <h2>Wallet</h2>
-
         <input class='MyCode' type='tel' maxlength='5' placeholder='Enter Code' />
-
         <button class='forestgreen'>Sign In</button>
-    
     `);
 
-    CLICKED('.forestgreen',()=>{
+    CLICKED('.forestgreen', () => {
 
-        const Code=document.querySelector('.MyCode');
+        const Code = document.querySelector('.MyCode').value;
 
-        if(!Code.value){
-
-            MESSAGE('Enter Code');
-
-            return
-        }
-
-
-        if (Code.value !== '05012' || Code.value !==  '20011' ) {
-
-            MESSAGE('Wrong COde');
-
+        if (Code !== '05012' && Code !== '20011') {
+            MESSAGE('Wrong Code');
             return;
-            
         }
 
-        if (Code ==='05012') {
-
-            localStorage.setItem('User','01');
-
-            localStorage.setItem('Name','ErouAndrewRichard');
-
-            localStorage.setItem('Email','erouandrewrichard01@gmail.com');
-
+        if (Code === '05012') {
+            localStorage.setItem('User', '01');
+            localStorage.setItem('Name', 'ErouAndrewRichard');
+            localStorage.setItem('Email', 'erouandrewrichard01@gmail.com');
             HOMEPAGE();
-
             return;
-            
         }
 
-        if (Code.value ==='20011') {
-
-            localStorage.setItem('User','02');
-
-            localStorage.setItem('Name','NagamiEstherRuth');
-
-            localStorage.setItem('Email','nagamiestherruth@gmail.com');
-
+        if (Code === '20011') {
+            localStorage.setItem('User', '02');
+            localStorage.setItem('Name', 'NagamiEstherRuth');
+            localStorage.setItem('Email', 'nagamiestherruth@gmail.com');
             HOMEPAGE();
-
             return;
-            
         }
 
-
-    })
-
+    });
 }
